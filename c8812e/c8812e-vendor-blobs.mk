3a6ef56c88dc581a4228d8c5bd7ec3e421f8b227
huangqiwu <huangqiwu@shendu.com>
2013-01-18 15:17:28 +0800

    use msm7x27a

--- c8812e/c8812e-vendor-blobs.mk
+++ c8812e/c8812e-vendor-blobs.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/c8812e/proprietary/bin/rmt_oeminfo:system/bin/rmt_oeminfo \
     vendor/huawei/c8812e/proprietary/bin/v4l2-qcamera-app:system/bin/v4l2-qcamera-app \
     vendor/huawei/c8812e/proprietary/bin/write_NV_114:system/bin/write_NV_114 \
-    vendor/huawei/c8812e/proprietary/lib/hw/camera.msm7627a.so:system/lib/hw/camera.msm7627a.so \
+    vendor/huawei/c8812e/proprietary/lib/hw/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so \
     vendor/huawei/c8812e/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
     vendor/huawei/c8812e/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
     vendor/huawei/c8812e/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
